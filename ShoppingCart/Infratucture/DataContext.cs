16a246c6574256cb7111d634d6c6a26a6c7cb99f
mijan44 <82171609+mijan44@users.noreply.github.com>
2022-11-19 00:17:03 +0800

    Add project files.

---
+++ ShoppingCart/Infratucture/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+
+
+namespace ShoppingCart.Infratucture
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
